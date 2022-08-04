76814c5f1f0ebf9abe1081e0924930c7477b70e4
luanadl <luanadl@ciandt.com>
2022-08-04 11:55:34 -0300

    Internationalization for RESTful Services 2

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -39,11 +40,12 @@ public class HelloWorldController {
 
     @GetMapping(path = "/hello-world-internationalized")
     public String helloWorldInternationalized(
-            @RequestHeader(name="Accept-Language", required = false) Locale locale
+            //@RequestHeader(name="Accept-Language", required = false) Locale locale
             ) {
         return messageSource
-                .getMessage("good.morning.message", null,"Default Message", locale);
-
+                .getMessage("good.morning.message", null,"Default Message",
+                        //locale);
+                        LocaleContextHolder.getLocale());
 
         // EN = Hello World
         // DE = Hallo Welt
